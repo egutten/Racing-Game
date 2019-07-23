c6117c96f41e22ab25af0724c0f134c7cf1eebb6
Marshall Guttenberg <mguttenberg@healthright.com>
2019-07-23 11:51:22 -0700

    time functionality for one car achieved

---
+++ app.js
@@ -0,0 +1,49 @@
+function Car(maxSpeed, curveSpeed){
+  this.maxSpeed = maxSpeed;
+  this.curveSpeed = curveSpeed;
+}
+
+function Track(straightDistance, curveDistance){
+  this.straightDistance = straightDistance;
+  this.curveDistance = curveDistance;
+}
+
+var track = new Track();
+var car = new Car();
+
+carConfig();
+trackConfig();
+
+var calculate = document.querySelector("#calculate");
+calculate.addEventListener("click", function(){
+  calculateTime();
+});
+
+function carConfig(){
+  var maxSpeedInput = document.querySelector("#max-speed");
+  maxSpeedInput.addEventListener("change", function(){
+    car.maxSpeed = Number(maxSpeedInput.value);
+  });
+  var curveSpeedInput = document.querySelector("#curve-speed");
+  curveSpeedInput.addEventListener("change", function(){
+    car.curveSpeed = Number(curveSpeedInput.value);
+  });
+}
+
+function trackConfig(){
+  var straightDistanceInput = document.querySelector("#straight-distance");
+  straightDistanceInput.addEventListener("change", function(){
+    track.straightDistance = Number(straightDistanceInput.value);
+  });
+  var curveDistanceInput = document.querySelector("#curve-distance");
+  curveDistanceInput.addEventListener("change", function(){
+    track.curveDistance = Number(curveDistanceInput.value);
+  });
+}
+
+function calculateTime(){
+  var straightTime = track.straightDistance / car.maxSpeed;
+  var curveTime = track.curveDistance/ car.curveSpeed;
+  var totalTime = straightTime + curveTime;
+  console.log (Math.round(totalTime));
+}
