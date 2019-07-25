492c8885521620ae056f3e609d089e792e2be318
Marshall Guttenberg <mguttenberg@healthright.com>
2019-07-25 10:38:04 -0700

    Rearrange app as suggested to learn structure

--- app.js
+++ app.js
@@ -1,110 +1,106 @@
 function Obj(maxSpeed, curveSpeed){
   this.maxSpeed = maxSpeed;
   this.curveSpeed = curveSpeed;
-  this.name = name;
+  this.name = name || "";
+  this.reset = function(){
+    this.maxSpeed = "";
+    this.curveSpeed = "";
+    this.name = "";
+  }
 }
 
 function Track(straightDistance, curveDistance){
   this.straightDistance = straightDistance;
   this.curveDistance = curveDistance;
+  this.reset = function(){
+    this.straightDistance = "";
+    this.curveDistance = "";
+  }
+  this.calculateTime = function(obj) {
+      var straightTime = this.straightDistance / obj.maxSpeed;
+      var curveTime = this.curveDistance/ obj.curveSpeed;
+      var totalTime = straightTime + curveTime;
+      return totalTime;
+  }
   this.race = function race(obj1, obj2){
-    var race = document.querySelector("#race");
-    race.addEventListener("click", function(){
-        compareObj(obj1, obj2);
-        document.querySelector(".new-race").classList.add("reveal");
-    });
+    var obj1Time = this.calculateTime(obj1);
+    var obj2Time = this.calculateTime(obj2);
+    if (obj1Time === obj2Time) {
+      return false;
+    } else if (obj1Time < obj2Time) {
+      return obj1;
+    } else {
+      return obj2;
+    }
   };
-  this.newRace = function newRace(obj1, obj2) {
-    var newRace = document.querySelector(".new-race");
-    var input = document.querySelectorAll("input[type='text']");
-    newRace.addEventListener("click", function(){
-      resetObj(obj1, obj2);
-      resetTrack();
-      input.forEach (function(i){
+}
+
+function App(){
+  this.init = function() {
+    this.track = new Track();
+    this.car1 = new Obj();
+    this.car2 = new Obj();
+    this.addListeners();
+  }
+  
+  this.selectors = {
+    $raceButton: document.querySelector("#race"),
+    $newRaceButton: document.querySelector(".new-race"),
+    $input: document.querySelectorAll("input[type='text']"),
+    $winner: document.querySelector("#winner-box"),
+    $straightDistanceInput: document.querySelector("#straight-distance"),
+    $curveDistanceInput: document.querySelector("#curve-distance")
+  }  
+  
+  this.addListeners = function(){
+    // Configure the cars
+    this.addCarListeners(this.car1, document.querySelector("#max-speed1"), document.querySelector("#curve-speed1"), document.querySelector("#name1"));
+    this.addCarListeners(this.car2, document.querySelector("#max-speed2"), document.querySelector("#curve-speed2"), document.querySelector("#name2"));
+    
+    // Configure the track
+    this.selectors.$straightDistanceInput.addEventListener("change", function(){
+      this.track.straightDistance = Number(this.selectors.$straightDistanceInput.value);
+    }.bind(this));
+    this.selectors.$curveDistanceInput.addEventListener("change", function(){
+      this.track.curveDistance = Number(this.selectors.$curveDistanceInput.value);
+    }.bind(this));
+    
+    // Race cars and display the winner
+    this.selectors.$raceButton.addEventListener("click", function(){
+      var winner = this.track.race(this.car1, this.car2);
+      if (!winner) {
+        this.selectors.$winner.innerHTML = "It's a tie!";
+      } else {
+        this.selectors.$winner.innerHTML = "And the winner is... " + winner.name + "!";
+      }
+      this.selectors.$newRaceButton.classList.add("reveal");
+    }.bind(this));
+    
+    // Reset and race again with a new race
+    this.selectors.$newRaceButton.addEventListener("click", function(){
+      this.car1.reset();
+      this.car2.reset();
+      this.track.reset();
+      this.selectors.$input.forEach(function(i){
         i.value = "";
       });
-      document.querySelector("#winner-box").innerHTML = "";
-      newRace.classList.remove("reveal");
+      this.selectors.$winner.innerHTML = "";
+      this.selectors.$newRaceButton.classList.remove("reveal");
+    }.bind(this));
+  }
+  
+  this.addCarListeners = function (obj, $maxSpeed, $curveSpeed, $name) {
+    $maxSpeed.addEventListener("change", function(){
+      obj.maxSpeed = Number(this.value);
     });
-  };
+    $curveSpeed.addEventListener("change", function(){
+      obj.curveSpeed = Number(this.value);
+    });
+    $name.addEventListener("change", function(){
+      obj.name = this.value;
+    });
+  }    
 }
 
-var track = new Track();
-var car1 = new Obj();
-var car2 = new Obj();
-
-objConfig(car1, car2);
-trackConfig();
-track.race(car1, car2);
-track.newRace(car1, car2);
-
-// Setting the obj's specks based on what the user enters into the inputs
-function objSet(obj, input_a, input_b, input_c) {
-  var maxSpeedInput = document.querySelector(input_a);
-  maxSpeedInput.addEventListener("change", function(){
-    obj.maxSpeed = Number(maxSpeedInput.value);
-  });
-  var curveSpeedInput = document.querySelector(input_b);
-  curveSpeedInput.addEventListener("change", function(){
-    obj.curveSpeed = Number(curveSpeedInput.value);
-  });
-  var nameInput = document.querySelector(input_c);
-  nameInput.addEventListener("change", function(){
-    obj.name = nameInput.value;
-  });
-}
-
-function objConfig(obj1, obj2){
-  objSet(obj1, "#max-speed1", "#curve-speed1", "#name1");
-  objSet(obj2, "#max-speed2", "#curve-speed2", "#name2");
-}
-
-// Setting the track's specs based on what the user enters into the inputs
-function trackConfig(){
-  var straightDistanceInput = document.querySelector("#straight-distance");
-  straightDistanceInput.addEventListener("change", function(){
-    track.straightDistance = Number(straightDistanceInput.value);
-  });
-  var curveDistanceInput = document.querySelector("#curve-distance");
-  curveDistanceInput.addEventListener("change", function(){
-    track.curveDistance = Number(curveDistanceInput.value);
-  });
-}
-
-// Determine how long it takes each obj to travel the track based on specs
-function calculateTime(obj){
-    var straightTime = track.straightDistance / obj.maxSpeed;
-    var curveTime = track.curveDistance/ obj.curveSpeed;
-    var totalTime = straightTime + curveTime;
-    return totalTime;
-}
-
-// Compare the results of each obj against each other and generate a winner banner
-function compareObj(obj1, obj2){
-  var winnerBox = document.querySelector("#winner-box")
-  var obj1Time = calculateTime(obj1);
-  var obj2Time = calculateTime(obj2);
-  console.log(obj1Time, obj2Time);
-  if (obj1Time && obj2Time && obj1Time === obj2Time) {
-    winnerBox.innerHTML = "It's a tie!";
-  } else if (obj1Time && obj2Time && Math.min(obj1Time, obj2Time) === obj1Time) {
-    winnerBox.innerHTML = "And the winner is... " + obj1.name + "!";
-  } else if (obj1Time && obj2Time && Math.min(obj1Time, obj2Time) === obj2Time) {
-    winnerBox.innerHTML = "And the winner is... " + obj2.name + "!";
-  } 
-}
-
-// Reset functions
-function resetObj(obj1, obj2){
-  obj1.maxSpeed = "";
-  obj1.curveSpeed = "";
-  obj1.name = "";
-  obj2.maxSpeed = "";
-  obj2.curveSpeed = "";
-  obj2.name = "";
-}
-
-function resetTrack(){
-  track.straightDistance = "";
-  track.curveDistance = "";
-}
+var app = new App();
+app.init();
